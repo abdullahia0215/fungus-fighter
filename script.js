61340265a5229a59a3f1f63e1d6d62709048a31c
Abdi <abdullahia0215@gmail.com>
2024-05-06 18:57:39 -0500

    base and stretch goals completed

--- script.js
+++ script.js
@@ -5,91 +5,98 @@
 
 let fungusHP = 100;
 let userAP = 100;
-let fungusProgress = document.getElementById('hp-meter')
-let abilityProgress = document.getElementById('ap-meter')
+let fungusProgress = document.getElementById("hp-meter");
+let abilityProgress = document.getElementById("ap-meter");
+let fungusAnimation = document.querySelector(".freaky-fungus");
+let attackButtons = document.getElementsByClassName("attack-btn");
 
-function arcaneScepter (){
-    fungusHP -= 14
-    userAP -=12
-    fungusProgress.value -= 14
-    abilityProgress.value -= 12
-    renderGame();
-    console.log(fungusHP);
+function disableAttacks() {
+  for (let i = 0; i < attackButtons.length; i++) {
+    attackButtons[i].disabled = true;
+  }
 }
 
-function entangle (){
-    fungusHP -= 9
-    userAP -= 23
-    fungusProgress.value -= 9
-    abilityProgress.value -= 23
-    renderGame();
+function arcaneScepter() {
+  fungusHP -= 14;
+  userAP -= 12;
+  fungusProgress.value -= 14;
+  abilityProgress.value -= 12;
+  console.log(fungusAnimation);
+  renderGame();
+  console.log(fungusHP);
 }
 
-function dragonBlade (){
-    fungusHP -= 47
-    userAP -= 28
-    fungusProgress.value -= 47
-    abilityProgress.value -= 28
-    renderGame();
+function entangle() {
+  fungusHP -= 9;
+  userAP -= 23;
+  fungusProgress.value -= 9;
+  abilityProgress.value -= 23;
+  renderGame();
 }
 
-function starFire (){
-    fungusHP -= 25
-    userAP -= 33
-    fungusProgress.value -= 25
-    abilityProgress.value -= 33
-    shroomRegenerate();
-    renderGame();
+function dragonBlade() {
+  fungusHP -= 47;
+  userAP -= 28;
+  fungusProgress.value -= 47;
+  abilityProgress.value -= 28;
+  renderGame();
+}
+
+function starFire() {
+  fungusHP -= 25;
+  userAP -= 33;
+  fungusProgress.value -= 25;
+  abilityProgress.value -= 33;
+  renderGame();
 }
 
 function onReady() {
-    console.log("Ready to go!")
-    
-    // Make sure you check the index.html file! 
-    // There are lots of buttons and things ready for you to hook into here!
-    
-    
-    // 🧠 Remember
-    // - Handle events that ->
-    // - Updates state which is ->
-    // - Rendered to the DOM
+  console.log("Ready to go!");
 
+  // Make sure you check the index.html file!
+  // There are lots of buttons and things ready for you to hook into here!
+
+  // 🧠 Remember
+  // - Handle events that ->
+  // - Updates state which is ->
+  // - Rendered to the DOM
 }
 
-// function shroomRegenerate (){
-// if (fungusHP < 50){
-//     fungusHP++;
-//     fungusProgress.value++;
-//     setInterval(shroomRegenerate, 1000)
-// } else if (fungusHP >= 50){
-//     clearInterval(shroomRegenerateInterval)
-//     // do nothing
-// }
-// }
-
-
-function shroomRegenerate (){
-    if (fungusHP > 0 && fungusHP < 50){
-        fungusHP++;
-        fungusProgress++;
-        renderGame();
-
-    } else if (fungusHP >= 50){
-        clearInterval()
-        renderGame();
-    }
+function shroomRegenerate() {
+  if (fungusHP > 0 && fungusHP < 50) {
+    fungusHP++;
+    fungusProgress.value++;
+    renderGame();
+  } else if (fungusHP >= 50) {
+    clearInterval();
+    renderGame();
+  }
 }
 
-function startShroomRegen (){
-    shroomRegenerate = setInterval(shroomRegenerate, 1000)
+function startShroomRegen() {
+  shroomRegenerate = setInterval(shroomRegenerate, 1000);
+}
+function gameResult() {
+  if (fungusHP <= 0) {
+    fungusHP = 0;
+    disableAttacks();
+    fungusAnimation.classList.remove("walk");
+    fungusAnimation.classList.add("dead");
+  } else if (userAP <= 0) {
+    userAP = 0;
+    disableAttacks();
+    fungusAnimation.classList.remove("walk");
+    fungusAnimation.classList.add("jump");
+  }
 }
 
-function renderGame (){
-    startShroomRegen();
-    const shroomHP = document.querySelector('.hp-text')
-    const playerAP = document.querySelector('.ap-text')
-    shroomHP.textContent = `${fungusHP} HP`
-    playerAP.textContent = `${userAP} AP`
+function renderGame() {
+  gameResult();
+  startShroomRegen();
+  const shroomHP = document.querySelector(".hp-text");
+  const playerAP = document.querySelector(".ap-text");
+  shroomHP.textContent = `${fungusHP} HP`;
+  playerAP.textContent = `${userAP} AP`;
 }
 
-onReady()
+onReady();
