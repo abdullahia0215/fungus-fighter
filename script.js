feb844eef12537d2f14aedb6ee8cf00b6a152582
Abdi <abdullahia0215@gmail.com>
2024-05-06 17:29:53 -0500

    fungus now starts regenerating if under 50 hp but also above 0 hp

--- script.js
+++ script.js
@@ -3,6 +3,45 @@
 // Example:
 // let fungusHP = 100;
 
+let fungusHP = 100;
+let userAP = 100;
+let fungusProgress = document.getElementById('hp-meter')
+let abilityProgress = document.getElementById('ap-meter')
+
+function arcaneScepter (){
+    fungusHP -= 14
+    userAP -=12
+    fungusProgress.value -= 14
+    abilityProgress.value -= 12
+    renderGame();
+    console.log(fungusHP);
+}
+
+function entangle (){
+    fungusHP -= 9
+    userAP -= 23
+    fungusProgress.value -= 9
+    abilityProgress.value -= 23
+    renderGame();
+}
+
+function dragonBlade (){
+    fungusHP -= 47
+    userAP -= 28
+    fungusProgress.value -= 47
+    abilityProgress.value -= 28
+    renderGame();
+}
+
+function starFire (){
+    fungusHP -= 25
+    userAP -= 33
+    fungusProgress.value -= 25
+    abilityProgress.value -= 33
+    shroomRegenerate();
+    renderGame();
+}
+
 function onReady() {
     console.log("Ready to go!")
     
@@ -14,7 +53,43 @@ function onReady() {
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+
 }
 
+// function shroomRegenerate (){
+// if (fungusHP < 50){
+//     fungusHP++;
+//     fungusProgress.value++;
+//     setInterval(shroomRegenerate, 1000)
+// } else if (fungusHP >= 50){
+//     clearInterval(shroomRegenerateInterval)
+//     // do nothing
+// }
+// }
+
+
+function shroomRegenerate (){
+    if (fungusHP > 0 && fungusHP < 50){
+        fungusHP++;
+        fungusProgress++;
+        renderGame();
+
+    } else if (fungusHP >= 50){
+        clearInterval()
+        renderGame();
+    }
+}
+
+function startShroomRegen (){
+    shroomRegenerate = setInterval(shroomRegenerate, 1000)
+}
+
+function renderGame (){
+    startShroomRegen();
+    const shroomHP = document.querySelector('.hp-text')
+    const playerAP = document.querySelector('.ap-text')
+    shroomHP.textContent = `${fungusHP} HP`
+    playerAP.textContent = `${userAP} AP`
+}
 
 onReady()
